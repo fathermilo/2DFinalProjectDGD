0d073a4c4c4e67d16b78357b0b20e3712d54c0d6
fathermilo <143011687+fathermilo@users.noreply.github.com>
2024-05-07 09:27:40 -0700

    Day 3 Continue
    - No new features added
    - just modifications of enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[] enemyPrefab;
-    private float spawnRange = 9.0f;
+    private float spawnRange = 10.0f;
     public int enemyCount;
     public int waveNumber = 1;
     
@@ -39,9 +39,9 @@ public class SpawnManager : MonoBehaviour
     private Vector3 GenerateSpawnPosition()
     {
         // Randomized the enemy's spawnpoint in the game
+        float spawnPosY = Random.Range(8, spawnRange);
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, spawnPosY, 0);
         return randomPos;
     }
 
