6e43283e086615ddd9d4d39e758a45f00ae75801
fathermilo <143011687+fathermilo@users.noreply.github.com>
2024-05-13 09:19:10 -0700

    Day 5 Personal Project
    Enemies are able to rotate towards the player and shooting

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float rotationModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 vectorToTarget = player.transform.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+    }
+}
